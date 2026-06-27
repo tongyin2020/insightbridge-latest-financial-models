2e251fde4d41c4612a1a8ee98754d7dddd03b62b
tongyin2020 <tongyin2020@github.com>
2026-06-27 01:37:24 -0500

    Revert "Add massive-history IBM quantum runner and latest validation outputs"
    This reverts commit eec3406dcd68b7760d4f2d1d604a76c8e3a275d0.

--- run_eventalpha_massive_history_quantum_once.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-BASE="/Users/tongyin/Desktop/InsightBridge_Financial_Models_Latest"
-PYTHON_BIN="${PYTHON_BIN:-/opt/anaconda3/bin/python3}"
-
-"$PYTHON_BIN" "$BASE/run_eventalpha_massive_history_quantum_once.py" "$@"
